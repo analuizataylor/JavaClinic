96b7fc776b89de70129eb1e0deb4b58d0cf13e00
Ana Luiza Menelli Taylor <analuizamenelli@gmail.com>
2025-02-17 13:26:34 -0300

    Delete src/main/java/br/faesa/javaclinic directory

--- src/main/java/br/faesa/javaclinic/service/validator/ValidacaoException.java
+++
@@ -1,7 +0,0 @@
-package br.faesa.javaclinic.service.validator;
-
-public class ValidacaoException extends RuntimeException {
-    public ValidacaoException(String mensagem) {
-        super(mensagem); // Construtor que passa a mensagem de erro para a classe pai RuntimeException
-    }
-}
